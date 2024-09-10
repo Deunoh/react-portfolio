b366f88f862b83d8167e4c3925b1a8e0763af3c9
deunoh <denovann@live.fr>
2024-09-10 15:25:36 +0200

    about me WIP

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -1,8 +1,49 @@
+import Socialbar from '../Socialbar/Socialbar';
 import './Presentation.scss';
 
 const Presentation = () => {
   return (
-    <section className="presentation-section">ICI Prochaine section</section>
+    <section className="presentation-section">
+      <h2 className="about_main-title">Qui suis-je ?</h2>
+      <div className="about_container">
+        <div className="about_container-card">
+          <h3>Passé</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+            quisquam suscipit necessitatibus in deleniti dolore reiciendis
+            temporibus possimus, fugiat nulla a autem fuga? Illo vero laborum
+            nobis, natus sit, labore fuga, qui ex aliquid hic nihil nostrum
+            laboriosam blanditiis quis ab vitae quod perspiciatis veritatis.
+            Perferendis sint hic qui aut!
+          </p>
+        </div>
+        <div className="about_container-card">
+          <h3>Présent</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+            quisquam suscipit necessitatibus in deleniti dolore reiciendis
+            temporibus possimus, fugiat nulla a autem fuga? Illo vero laborum
+            nobis, natus sit, labore fuga, qui ex aliquid hic nihil nostrum
+            laboriosam blanditiis quis ab vitae quod perspiciatis veritatis.
+            Perferendis sint hic qui aut!
+          </p>
+        </div>
+        <div className="about_container-card">
+          <h3>Futur</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+            quisquam suscipit necessitatibus in deleniti dolore reiciendis
+            temporibus possimus, fugiat nulla a autem fuga? Illo vero laborum
+            nobis, natus sit, labore fuga, qui ex aliquid hic nihil nostrum
+            laboriosam blanditiis quis ab vitae quod perspiciatis veritatis.
+            Perferendis sint hic qui aut!
+          </p>
+        </div>
+      </div>
+      <div className="social-icons--container">
+        <Socialbar />
+      </div>
+    </section>
   );
 };
 
